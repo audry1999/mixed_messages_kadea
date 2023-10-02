86303eac198638a73c8a15900bca5f83b83a16aa
audry1999 <audrykaj@gamil.com>
2023-10-02 16:40:23 +0200

    dernier commit

--- index.js
+++ index.js
@@ -1,27 +1,21 @@
-// Générer un index aléatoire dans le tableau
-const randomIndex = Math.floor(Math.random() * messages.length);
-  
-// Obtenez l'objet de message aléatoire
-const randomMessage = messages[randomIndex];
 
-// Sélectionnez au hasard un mot dans chaque liste dans l'objet du message
-const randomSubject = randomMessage.subject;
-const randomAdjective = randomMessage.adjectives[Math.floor(Math.random() * randomMessage.adjectives.length)];
-const randomVerbIndex = Math.floor(Math.random() * randomMessage.verbs.length);
-const randomVerb = randomMessage.verbs[randomVerbIndex];
-const randomObject = randomMessage.objects[Math.floor(Math.random() * randomMessage.objects.length)];
+const Messages = [
+  { 
+    Nom: ["emmanuel", "audry", "bienv", "justin",], 
+  },
+  { 
+    Verbe: ["est", "a", "pouvait etre", "peux",], 
+  },
+  { 
+    Adjectif: ["gentil", "arogant", "malade", "malin",], 
+  },
 
-let randomAdverbialPhrase;
-if (Array.isArray(randomMessage.adverbialPhrases)) {
-  const randomIndex = Math.floor(Math.random() * randomMessage.adverbialPhrases.length);
-  randomAdverbialPhrase = randomMessage.adverbialPhrases[randomIndex];
-} else {
-  randomAdverbialPhrase = randomMessage.adverbialPhrases;
-}
+];
+const randomIndex = Math.floor(Math.random() * Messages.length)
+const randomMessage = Messages[randomIndex];
+ const randomNoms =  randomMessage.Nom[Math.floor(Math.random() * randomMessage.Nom.length)];
+const randomVerbes = randomMessage.Verbe[Math.floor(Math.random() * randomMessage.Verbe.length)];
+const randomAdjectifs =  randomMessage.Adjectif[Math.floor(Math.random() * randomMessage.Adjectif.length)];
+let message = randomNoms + " " + randomVerbes + " " + randomAdjectifs + ".";
 
-// Construire le message aléatoire
-let message = randomSubject + " " + randomVerb + " " + randomAdjective + " and " + randomObject + " " + ".";
-
-// Afficher le message aléatoire
 console.log(message);
-
